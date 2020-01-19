bd2eb588a6f284087ee33481bc6ed520b1d0c91b
hanxiangyi <a1585475775@qq.com>
2020-01-19 22:09:41 +0800

    首页设置

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,39 @@
+<template>
+	<tab-bar>
+		<tab-bar-item path="/home" >
+			<img slot="item-icon" src="~assets/img/tabber/home.svg"/>
+			<img slot="item-icon-active" src="~assets/img/tabber/active-home.svg"/>
+			<div slot="item-text">首页</div>
+		</tab-bar-item>
+		<tab-bar-item path="/category" >
+			<img slot="item-icon" src="~assets/img/tabber/classify.svg"/>
+			<img slot="item-icon-active" src="~assets/img/tabber/active-classify.svg"/>
+			<div slot="item-text">分类</div>
+		</tab-bar-item>
+		<tab-bar-item path="/cart" >
+			<img slot="item-icon" src="~assets/img/tabber/shop.svg"/>
+			<img slot="item-icon-active" src="~assets/img/tabber/active-shop.svg"/>
+			<div slot="item-text">购物车</div>
+		</tab-bar-item>
+		<tab-bar-item path="/profile" >
+			<img slot="item-icon" src="~assets/img/tabber/me.svg"/>
+			<img slot="item-icon-active" src="~assets/img/tabber/active-me.svg"/>
+			<div slot="item-text">我的</div>
+		</tab-bar-item>
+	</tab-bar>
+</template>
+
+<script>
+	import TabBar from 'components/common/tabber/TabBar'
+	import TabBarItem from 'components/common/tabber/TabBarItem'
+	export default{
+		name:"MainTabBar",
+		components:{
+			TabBar,
+			TabBarItem
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/network/request.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+export function request(config) {
+    const instance = axios.create({
+       baseURL:'http://106.54.54.237:8000/api/hy',
+       timeout: 5000
+    })
+// axios拦截器
+// 请求拦截的作用:
+// 比如config中的一些消息不符合服务器的要求
+// 比如每次发送网络请求时，都希望在界面中显示一个请求
+// 比如某些网络请求（登录（token）），必须携带一些特殊的信息
+instance.interceptors.request.use(config =>{
+  console.log(config);
+  return config
+}), err =>{
+  console.log(err);
+}
+//相应拦截
+instance.interceptors.response.use(res =>{
+  return res.data
+}),err =>{
+  console.log(err);
+}
+
+
+//发送真正的网络请求，instance会返回一个promise
+    return instance(config)
+}
+
